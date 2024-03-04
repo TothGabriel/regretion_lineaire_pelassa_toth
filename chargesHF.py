756f487501424c2580b03285955279970e275c7e
Vilguax <axel.pelassa@hotmail.fr>
2024-03-04 11:06:16 +0100

    Utilisation du "sex" comme donnée continue

--- chargesHF.py
+++ chargesHF.py
@@ -1,45 +1,45 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
-# Dataset
 donnees = pd.read_csv('insurance.csv')
-donnees_simplifiees = donnees[['age', 'charges']]
 
-# Séparer les données entre hommes et femmes
-donnees_hommes = donnees[donnees['sex'] == 'male']
-donnees_femmes = donnees[donnees['sex'] == 'female']
+# Encodage de la variable 'sex'
+donnees['sex'] = donnees['sex'].map({'female': 0, 'male': 1})
 
-# Préparation des données pour la régression
-X_hommes = donnees_hommes[['age']]
-y_hommes = donnees_hommes['charges']
-X_femmes = donnees_femmes[['age']]
-y_femmes = donnees_femmes['charges']
+# Sélection et préparation des données
+X = donnees[['age', 'sex']]
+y = donnees['charges']
 
-# Entraînement des modèles de régression linéaire
-modele_hommes = LinearRegression().fit(X_hommes, y_hommes)
-modele_femmes = LinearRegression().fit(X_femmes, y_femmes)
-y_pred_hommes = modele_hommes.predict(X_hommes)
-y_pred_femmes = modele_femmes.predict(X_femmes)
+# Séparation des données entre hommes et femmes pour la visualisation
+donnees_hommes = donnees[donnees['sex'] == 1]
+donnees_femmes = donnees[donnees['sex'] == 0]
+
+# Entraînement du modèle
+modele = LinearRegression().fit(X, y)
+
+# Prédiction
+y_pred = modele.predict(X)
 
 # Visualisation
 plt.figure(figsize=(10, 6))
 
-# Données et lignes de régression pour les hommes
-plt.scatter(X_hommes['age'], y_hommes, color='red', label='Hommes', alpha=0.5)
-indices_sorted_hommes = X_hommes['age'].argsort()
-plt.plot(X_hommes['age'].iloc[indices_sorted_hommes], y_pred_hommes[indices_sorted_hommes], color='violet', label='Ligne Hommes')
+plt.scatter(donnees_hommes['age'], donnees_hommes['charges'], color='red', label='Hommes', alpha=0.5)
+plt.scatter(donnees_femmes['age'], donnees_femmes['charges'], color='blue', label='Femmes', alpha=0.5)
 
+# Séparation des prédictions entre hommes et femmes
+y_pred_hommes = modele.predict(donnees_hommes[['age', 'sex']])
+y_pred_femmes = modele.predict(donnees_femmes[['age', 'sex']])
 
-# Données et lignes de régression pour les femmes
-plt.scatter(X_femmes['age'], y_femmes, color='blue', label='Femmes', alpha=0.5)
-indices_sorted_femmes = X_femmes['age'].argsort()
-plt.plot(X_femmes['age'].iloc[indices_sorted_femmes], y_pred_femmes[indices_sorted_femmes], color='green', label='Ligne Femmes')
+# Tri des données pour le tracé de la ligne
+indices_sorted_hommes = donnees_hommes['age'].argsort()
+indices_sorted_femmes = donnees_femmes['age'].argsort()
 
+plt.plot(donnees_hommes['age'].iloc[indices_sorted_hommes], y_pred_hommes[indices_sorted_hommes], color='violet', label='Ligne Hommes')
+plt.plot(donnees_femmes['age'].iloc[indices_sorted_femmes], y_pred_femmes[indices_sorted_femmes], color='green', label='Ligne Femmes')
 
-plt.title("Charges d'assurance par âge pour hommes et femmes")
+plt.title("Charges d'assurance par âge et sexe")
 plt.xlabel('Âge')
 plt.ylabel('Charges d\'assurance')
 plt.legend()
